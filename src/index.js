5ec8270aa722c1c50a1adf0e15484a9b54ca9d3e
Bára Valgerður Friðriksdóttir <bvf1@hi.is>
2018-11-27 04:49:22 +0000

    setti rollup-babel rétt upp, náði að nota AJAX fyrir forsíðuna, þurfti að breyta lectures.json til að það virkaði -- þarf að laga,

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import List from './lib/list';
+// import List from './lib/list';
 
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
   const isLecturePage = page.classList.contains('lecture-page');
 
-  if (isLecturePage) {
-
+ /* if (isLecturePage) {
+    //
   } else {
     const list = new List();
     list.load();
   }
+*/ 
+
+  fetch('../lectures.json')
+    .then((result) => {
+      if (!result.ok) {
+        throw new Error('Non 200 status');
+      }
+      return result.json();
+    })
+    .then((data) => {
+      // console.log(data);
+      let output = '';
+      data.forEach((lecture) => {
+        output += `
+        <div class="lecture">
+          <p>${lecture.thumbnail}</p>
+          <p>${lecture.category}</p>
+          <p>${lecture.title}</p>
+        </div>
+      `;
+      });
+      document.getElementById('lectures').innerHTML = output;
+    })
+    .catch(error => console.error(error));
 });
