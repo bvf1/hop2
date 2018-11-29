ee1b996a51b4bc241a5737efe5d631d0fab0419c
Bára Valgerður Friðriksdóttir <bvf1@hi.is>
2018-11-29 01:45:22 +0000

    kláraði html fyrir fyrirlestur, held ég

--- src/lib/fyrirlestur.js
+++ src/lib/fyrirlestur.js
@@ -2,13 +2,14 @@ const x = location.search;
 
 const slug = x.substring(6);
 
+const output = document.getElementById('output');
+let lists = -1;
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const page = document.querySelector('body');
 
-  function list() {
-    console.log("listi");
-    //lecture.list
-  }
+  
 
   function setHeader(image, category, title) {
     let output = '';
@@ -21,8 +22,14 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function element(type, data) {
-
     let el;
+    //const output = document.getElementById('output');
+    let li;
+    
+    let text;
+    let item;
+    
+
     switch (type) {
       case 'youtube':
         el = document.createElement('video');
@@ -30,32 +37,46 @@ document.addEventListener('DOMContentLoaded', () => {
       case 'text':
         el = document.createElement('p');
         break;
+      case 'attribute':
+        el = document.createElement('attribute');
+        break;
       case 'quote':
         el = document.createElement('blockquote');
         break;
       case 'image':
         el = document.createElement('img');
         break;
+      case 'caption':
+        el = document.createElement('caption');
+        break;
       case 'heading':
         el = document.createElement('h2');
         break;
       case 'list':
-        //list();
-        el = document.createElement('p');
-        break;
+        lists += 1;
+        el = document.createElement('ul');
+        el.setAttribute('class', 'list');
+        output.appendChild(el);
+        return;
+      case 'li':
+        li = document.createElement('li');
+        text = document.createTextNode(data);
+        li.appendChild(text);      
+
+        item = document.getElementsByClassName('list');
+        item[lists].appendChild(li);
+        return;
       case 'code':
-        el = document.createElement('video');
+        el = document.createElement('code');
         break;
       default:
         console.log(undefined);
         break;
     }
-
-    //console.log(el);
-    const output = document.getElementById('output');
-    const text = document.createTextNode(data);
+    text = document.createTextNode(data);
     output.appendChild(el).appendChild(text);
   }
+
 
   fetch('../lectures.json')
     .then((result) => {
@@ -78,15 +99,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const contents = chosenLecture.content;
 
-      // console.log(contents);
-
       let lectureType;
       let lectureData;
       for (let i = 0; i < contents.length; i += 1) {
         lectureType = contents[i].type;
         lectureData = contents[i].data;
-
+        // console.log(lectureType);
+        // console.log(lectureData[0]);
+        // console.log(lectureData.length);
         element(lectureType, lectureData);
+
+        if (lectureType === 'list') {
+          for (let j = 0; j < lectureData.length; j += 1) {
+            element('li', lectureData[j]);
+          }
+        }
+
+        if (contents[i].attribute) {
+          lectureData = contents[i].attribute;
+          element('attribute', lectureData);
+        }
+        if (contents[i].caption) {
+          lectureData = contents[i].caption;
+          element('caption', lectureData);
+        }
       }
     })
     .catch(error => console.error(error));
